55c82b53ca9842595ae9e39180fe65d5eaa3e01d
Esraa Ali <esraaaliiii4@gmail.com>
2023-06-24 19:31:20 +0300

    add commonly used files

---
+++ Lib/Utils.h
@@ -0,0 +1,12 @@
+/**
+ * Utils.h
+ *
+ *  Created on: Sun Mar 26 2023
+ */
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "Std_Types.h"
+#define REG32(BASE_ADDR, OFFSET)  (*(uint32 *)((BASE_ADDR) + (OFFSET)))
+
+#endif /* UTILS_H */
